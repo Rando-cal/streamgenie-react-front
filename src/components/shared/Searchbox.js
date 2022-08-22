9f52754de9d8d39d64af90e9fdd5c08b0e25df73
RandyD <rdsoundguy@gmai.com>
2022-08-22 10:46:11 -0400

    Adding searchbox to landing page

--- src/components/shared/Searchbox.js
+++ src/components/shared/Searchbox.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Searchbox = (props) => {
 	// const { msgAlert, user } = props
@@ -7,7 +9,22 @@ const Searchbox = (props) => {
 
 	return (
 		<>
-			<h2>Searchbox</h2>
+		<Form>
+      		<Form.Group className="mb-3" 
+					    controlId="formBasicEmail">
+        		<Form.Label>Email address</Form.Label>
+        		<Form.Control type="email" 
+				        	  placeholder="Enter email" />
+        		<Form.Text className="text-muted">
+          			
+        		</Form.Text>
+      		</Form.Group>
+
+			<Button variant="primary" type="submit">
+        		Submit
+      		</Button>
+		</Form>
+		
 		</>
 	)
 }
