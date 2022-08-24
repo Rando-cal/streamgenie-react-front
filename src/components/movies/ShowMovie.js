e094a1fe07a2e60104c2c66edb4497f99f8d6952
christian-the-brewer <brewer.christiang@gmail.com>
2022-08-24 13:08:37 -0400

    creating show movie component

--- src/components/movies/ShowMovie.js
+++ src/components/movies/ShowMovie.js
@@ -5,4 +5,82 @@ import { useParams, useNavigate } from 'react-router-dom'
 
 import { Container, Card, Button } from 'react-bootstrap'
 import LoadingScreen from '../shared/LoadingScreen'
-import { } from '../../api/movies.js'
+import { getOneMovie } from '../../api/movies.js'
+import messages from '../shared/AutoDismissAlert/messages'
+import { addToFavorites, removeFromFavorites } from "../../api/favorites";
+
+//the movie's id comes from the 3rd party API
+
+const ShowMovie = (props) => {
+    const [movie, setMovie] = useState(null)
+
+    const { id } = useParams()
+    const navigate = useNavigate()
+    const { user, msgAlert } = props
+
+    useEffect(() => {
+        getOneMovie(id)
+            .then(res => { return setMovie(res.data.movie), console.log("this is res.data.movie", res.data.movie) })
+            .catch(err => {
+                msgAlert({
+                    heading: 'Error getting movie',
+                    message: messages.getContentFailure,
+                    variant: 'danger'
+                })
+                navigate('/')
+                //navigate back to the home page if there's an error fetching
+            })
+    }, [])
+
+
+    //function to remove movie from the favorites list
+    const removeMovieFromFavorites = () => {
+        removeFromFavorites(user, movie.id)
+            // on success send a success message
+            .then(() => {
+                msgAlert({
+                    heading: 'Success',
+                    message: messages.removeContentSuccess,
+                    variant: 'success'
+                })
+            })
+            // then navigate to index
+            .then(() => { navigate('/') })
+            // on failure send a failure message
+            .catch(err => {
+                msgAlert({
+                    heading: 'Error removing movie',
+                    message: messages.removeContentFailure,
+                    variant: 'danger'
+                })
+            })
+    }
+
+
+    //function to add movie to favorites
+    const addToMovieToFavorites = () => {
+        addToFavorites(user, movie)
+            .then(() => {
+                msgAlert({
+                    heading: 'Success',
+                    message: messages.addContentSuccess,
+                    variant: 'success'
+                })
+            })
+
+            .then(() => { navigate('/') })
+
+            .catch(err => {
+                msgAlert({
+                    heading: 'Error adding movie to favorites',
+                    message: messages.removeContentFailure,
+                    variant: 'danger'
+                })
+            })
+    }
+
+    if (!movie) {
+        return <LoadingScreen />
+    }
+
+}
