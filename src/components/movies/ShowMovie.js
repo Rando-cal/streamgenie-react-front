8e2a8c56e685aa2062f0d8add74e7108ee17cc44
christian-the-brewer <brewer.christiang@gmail.com>
2022-08-24 10:32:35 -0400

    Adds api calls for movies and tv shows to get index of popular, by platform, by title, and a show for specific title with where it is streaming

---
+++ src/components/movies/ShowMovie.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import '../../public/index.css'
+
+import { useParams, useNavigate } from 'react-router-dom'
+
+import { Container, Card, Button } from 'react-bootstrap'
+import LoadingScreen from '../shared/LoadingScreen'
+import { } from '../../api/movies.js'
